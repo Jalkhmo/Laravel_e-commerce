9cd7370ada790ed90d6d81613ac210a620e5d53a
Aurélien Bizien <aurelienbizien@outlook.com>
2024-07-12 15:24:27 +0200

    checkout

--- la-cave-de-ton-grand-pere/resources/views/checkout.blade.php
+++ la-cave-de-ton-grand-pere/resources/views/checkout.blade.php
@@ -27,10 +27,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="#">La cave de ton Grand Pères</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <h1 class="navbar-brand" href="#">La cave de ton Grand Père</h1>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
